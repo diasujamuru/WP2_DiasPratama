c9caf1d94aa4c1880d8c623849b7695fa1662c5b
Jamuruu <diasspratamaa24@gmail.com>
2022-09-25 15:03:03 +0700

    initialized repo

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+	class Latihan1 extends CI_Controller {
+		public function index() {
+			echo "<h1>Halo nama saya Jamur</h1>";
+			echo "Kali ini saya akan belajar CodeIgniter ";
+		}
+
+		public function penjumlahan($n1, $n2) {
+			$this->load->model('Model_latihan1');
+			$data['nilai1'] = $n1;
+			$data['nilai2'] = $n2;
+			$data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+			$this->load->view("view_latihan1", $data);
+		}
+	}
+?>
